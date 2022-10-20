a0f8b263f23559249156a57dc11d902935849e12
Oemcristian <oemcristian@gmail.com>
2022-10-20 00:03:25 -0300

    criando em vue

---
+++ cristianfotografav2/src/App.vue
@@ -0,0 +1,53 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+
+  <nav class="p-5 text-white bg-slate-800/30 -mt-20 sticky top-0 z-50 md:flex md:items-center md:justify-between">
+    <span class="text-2xl font-[Poppins] font-bold">
+      Cristian Fotofraga
+    </span>
+
+    <ul class="md:flex md:items-center">
+      <li class="mx-4">
+        <a href="#" class="text-xl hover:text-cyan-500 duration-500">Início</a>
+      </li>
+      <li class="mx-4">
+        <a href="#" class="text-xl hover:text-cyan-500 duration-500">Início</a>
+      </li>
+      <li class="mx-4">
+        <a href="#" class="text-xl hover:text-cyan-500 duration-500">Início</a>
+      </li>
+      <li class="mx-4">
+        <a href="#" class="text-xl hover:text-cyan-500 duration-500">Início</a>
+      </li>
+    </ul>
+</nav>
+<section class="section section-1">
+  <div class="header">
+  </div>
+</section>
+<section class="section section-2">
+    <div class="timeline">
+        <div class="linha">
+</div>
+        </div>
+        </section>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
