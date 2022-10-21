63fd75368caf395ae302150106dffc675fa7342c
Oemcristian <oemcristian@gmail.com>
2022-10-20 21:59:58 -0300

    configurando

--- cristianfotografav2/src/App.vue
+++ cristianfotografav2/src/App.vue
@@ -6,48 +6,30 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
 
-  <nav class="p-5 text-white bg-slate-800/30 -mt-20 sticky top-0 z-50 md:flex md:items-center md:justify-between">
+  <nav class="p-5 text-white bg-slate-800 sticky top-0 z-50 md:flex md:items-center md:justify-between">
     <span class="text-2xl font-[Poppins] font-bold">
       Cristian Fotofraga
     </span>
-
     <ul class="md:flex md:items-center">
       <li class="mx-4">
         <a href="#" class="text-xl hover:text-cyan-500 duration-500">Início</a>
       </li>
       <li class="mx-4">
-        <a href="#" class="text-xl hover:text-cyan-500 duration-500">Início</a>
+        <a href="#" class="text-xl hover:text-cyan-500 duration-500">Portfólio</a>
       </li>
       <li class="mx-4">
-        <a href="#" class="text-xl hover:text-cyan-500 duration-500">Início</a>
-      </li>
-      <li class="mx-4">
-        <a href="#" class="text-xl hover:text-cyan-500 duration-500">Início</a>
+        <a href="#" class="text-xl hover:text-cyan-500 duration-500">Contato</a>
       </li>
     </ul>
 </nav>
-<section class="section section-1">
-  <div class="header">
-  </div>
-</section>
-<section class="section section-2">
-    <div class="timeline">
-        <div class="linha">
-</div>
-        </div>
-        </section>
+<header class="h-screen object-cover snap-proximity snap-y snap-start">
+  <img src="https://images.unsplash.com/photo-1542038784456-1ea8e935640e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" class="object-cover w-full h-full"/>    
+</header>
+    <div class="h-screen bg-white snap-proximity snap-y">
+      <div class="snap-start">
+      </div>
+    </div>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
